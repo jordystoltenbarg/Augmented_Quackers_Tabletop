cb85158144322c27fa953d3dbd05188a19ee36b0
Jordy Stoltenbarg <jord96@live.nl>
2020-10-13 15:45:48 +0200

    Copied Project

---
+++ AQ_ARTabletopProject/Assets/Scripts/Jordy_test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jordy_test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //I made this to do test stuffs. Yesh?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
