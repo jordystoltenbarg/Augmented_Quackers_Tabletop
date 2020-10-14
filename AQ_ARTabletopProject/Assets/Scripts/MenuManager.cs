229c3a134cef92185d982ddea2ebd479da401d58
Vasil Vasilev <wasilev.8v@gmail.com>
2020-10-14 16:45:36 +0200

    Working on UI
    + added PlayScreen
    + added PreLobbyScreen
    + added LobbyScreen
    + added SettingsScreen
    + added CreditsScreen

---
+++ AQ_ARTabletopProject/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Navigation Buttons")]
+    [SerializeField]
+    private Button _settingsButton;
+    [SerializeField]
+    private Button _backButton;
+
+    [Header("Slider")]
+    [SerializeField]
+    private GameObject _slider;
+    [SerializeField]
+    private GameObject _preLobbyContent;
+    [SerializeField]
+    private GameObject _lobbyContent;
+
+    [Header("Menu Screens")]
+    [SerializeField]
+    private GameObject _play;
+    [SerializeField]
+    private GameObject _collection;
+    [SerializeField]
+    private GameObject _preLobby;
+    [SerializeField]
+    private GameObject _lobby;
+    [SerializeField]
+    private GameObject _settings;
+    [SerializeField]
+    private GameObject _credits;
+
+
+    public enum MenuState
+    {
+        Play,
+        Collection,
+        PreLobby,
+        Lobby,
+        Settings,
+        Credits
+    }
+    public static MenuState CurrentMenuState;
+
+    void Start()
+    {
+        checkMenuState();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void checkMenuState()
+    {
+        if (_play.activeSelf)
+            CurrentMenuState = MenuState.Play;
+        else if (_collection.activeSelf)
+            CurrentMenuState = MenuState.Collection;
+        else if (_lobby.activeSelf)
+            CurrentMenuState = MenuState.PreLobby;
+        else if (_lobby.activeSelf)
+            CurrentMenuState = MenuState.Lobby;
+        else if (_settings.activeSelf)
+            CurrentMenuState = MenuState.Settings;
+        else if (_credits.activeSelf)
+            CurrentMenuState = MenuState.Credits;
+    }
+
+    public void OpenSlider()
+    {
+
+    }
+}
---
+++ AQ_ARTabletopProject/Assets/Scripts/SliderBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderBehaviour : MonoBehaviour
+{
+    public static bool ShouldOpenSlider;
+    public static bool ShouldCloseSlider;
+
+    private bool _isSliderOpen;
+
+    private RectTransform _rect;
+    private float _width;
+
+    void Start()
+    {
+        _rect = GetComponent<RectTransform>();
+        _width = transform.parent.GetComponent<RectTransform>().sizeDelta.x;
+        _rect.localPosition = new Vector2(-_width * 0.5f, _rect.localPosition.y);
+    }
+
+    void Update()
+    {
+        if (ShouldOpenSlider)
+        {
+            _rect.localPosition = Vector2.Lerp(new Vector2(_rect.localPosition.x, 0), new Vector2(_width * 0.5f, 0), Time.deltaTime * 10);
+        }
+    }
+}
