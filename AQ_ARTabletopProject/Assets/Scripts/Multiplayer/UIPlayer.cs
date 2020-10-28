1a937acadce1b6d3b95b923f9a505280d843f557
Jordy Stoltenbarg <jord96@live.nl>
2020-10-28 10:20:14 +0100

    Added networking features
    private match\public match/match searching/lobby's

---
+++ AQ_ARTabletopProject/Assets/Scripts/Multiplayer/UIPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayer : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    Player player;
+
+
+    public void SetPlayer (Player player)
+    {
+        this.player = player;
+        text.text = "Player " + player.playerIndex.ToString();
+    }
+}
