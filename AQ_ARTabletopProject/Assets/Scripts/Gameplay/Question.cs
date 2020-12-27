9f6186743354c516ca1919673b53d799f8dc6244
Vasil Vasilev <wasilev.8v@gmail.com>
2020-12-27 18:01:20 +0200

    Localizaion of questions

--- AQ_ARTabletopProject/Assets/Scripts/Gameplay/Question.cs
+++ AQ_ARTabletopProject/Assets/Scripts/Gameplay/Question.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 
 [CreateAssetMenu(fileName = "New Question", menuName = "Question")]
 public class Question : ScriptableObject
@@ -10,5 +11,26 @@ public class Question : ScriptableObject
     public string correctAnswer;
     public string quizTheme;
 
+    //The icon should be part of the object displaying the question
     public Sprite QuizIcon;
+
+    //Question
+    [SerializeField] private LocalizedString _localizedQuestion;
+    public LocalizedString LocalizedQuestion => _localizedQuestion;
+    //Wrong Answers
+    [SerializeField] private LocalizedString[] _localizedWrongAnswersArray;
+    public LocalizedString[] LocalizedWrongAnswersArray => _localizedWrongAnswersArray;
+    //Correct Asnwer
+    [SerializeField] private LocalizedString _localizedCorrectAnswer;
+    public LocalizedString LocalizedCorrectAnswer => _localizedCorrectAnswer;
+    //Type of Question
+    public enum Type
+    {
+        Nature,
+        History,
+        WaterTreatment,
+        Bats
+    }
+    [SerializeField] private Type _questionType;
+    public Type QuestionType => _questionType;
 }
